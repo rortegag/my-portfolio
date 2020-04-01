284d519aee8c34ab51422f6c21d5daa0636b4b45
M1lh0use <raulortega9@hotmail.com>
2020-04-01 19:21:22 +0200

    Updates

---
+++ precache-manifest.7719f8b1af1c92bf0fbe5ff2f85b667f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f87e8cafb5f96d0ff810ff453a907652",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "762f7b87ee7845d70a8c",
+    "url": "/my-portfolio/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "0b145f03ed41b6279c1d",
+    "url": "/my-portfolio/static/js/2.62c7a18f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-portfolio/static/js/2.62c7a18f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "762f7b87ee7845d70a8c",
+    "url": "/my-portfolio/static/js/main.d52041d5.chunk.js"
+  },
+  {
+    "revision": "164cd71dc8e56a933ce3",
+    "url": "/my-portfolio/static/js/runtime-main.d96a2d46.js"
+  },
+  {
+    "revision": "59daa1f8d998ed71c06d30fa1ba3e88d",
+    "url": "/my-portfolio/static/media/CV-OrtegaGomezRaul.59daa1f8.pdf"
+  }
+]);
